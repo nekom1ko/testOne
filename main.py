06f15e27d7b4e5e341fdb3a2ea03c867a42bd87a
Rostislav Solovyov <aqualungium@gmail.com>
2020-03-09 09:43:20 +0300

    new one

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import msvcrt
 
 # from pyinput import keyboard
 
@@ -8,27 +9,40 @@ def cls():
 
 
 class Field:
+    heroSymbol = "S"
+
     def __init__(self, w, h):
+        self.w = w
+        self.h = h
         cls()
         self.field = [[0 for x in range(w)] for y in range(h)]
 
+    # заполняю поле нулями
     def reset(self):
-        pass
+        self.field = [[0 for x in range(self.w)] for y in range(self.h)]
 
+    # очищаю консоль и заполняю поле заново
     def refresh(self, field):
         cls()
-        return "\n".join(
-            ["".join([format(item) for item in row]) for row in self.field]
+        print(
+            "\n".join(["".join([format(item) for item in row]) for row in self.field])
         )
 
     def spawnHero(self):
-        self.field[0][0] = "S"
-        print(self.refresh(self.field))
+        self.xPos = 0
+        self.yPos = 0
+        self.field[self.xPos][self.yPos] = self.heroSymbol
+        self.refresh(self.field)
 
-    def moving(self):
-        self.field
+    def moveRight(self):
+        self.xPos += 1
+        self.reset()
+        self.field[self.yPos][self.xPos] = self.heroSymbol
+        self.refresh(self.field)
 
 
 field = Field(20, 10)
 field.spawnHero()
+field.moveRight()
+field.moveRight()
 
