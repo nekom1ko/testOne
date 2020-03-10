368cce0da64650fd0e3de09a230ba03f78ebaa9d
Rostislav Solovyov <aqualungium@gmail.com>
2020-03-10 11:23:27 +0300

    new one

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import msvcrt
+import keyboard
+
 
 # from pyinput import keyboard
 
@@ -17,17 +19,18 @@ class Field:
         cls()
         self.field = [[0 for x in range(w)] for y in range(h)]
 
-    # заполняю поле нулями
+    # заполняю массив поля нулями
     def reset(self):
         self.field = [[0 for x in range(self.w)] for y in range(self.h)]
 
-    # очищаю консоль и заполняю поле заново
+    # очищаю консоль и рисую поле заново (передаю в функцию массив с полем)
     def refresh(self, field):
         cls()
         print(
             "\n".join(["".join([format(item) for item in row]) for row in self.field])
         )
 
+    # спавню героя на 0 0
     def spawnHero(self):
         self.xPos = 0
         self.yPos = 0
@@ -35,14 +38,42 @@ class Field:
         self.refresh(self.field)
 
     def moveRight(self):
-        self.xPos += 1
-        self.reset()
-        self.field[self.yPos][self.xPos] = self.heroSymbol
-        self.refresh(self.field)
+        if self.xPos != (self.w - 1):
+            self.xPos += 1
+            self.reset()
+            self.field[self.yPos][self.xPos] = self.heroSymbol
+            self.refresh(self.field)
+
+    def moveLeft(self):
+        if self.xPos != 0:
+            self.xPos -= 1
+            self.reset()
+            self.field[self.yPos][self.xPos] = self.heroSymbol
+            self.refresh(self.field)
 
 
-field = Field(20, 10)
-field.spawnHero()
-field.moveRight()
-field.moveRight()
+def startGame():
+    cls()
+    global field
+    field = Field(20, 10)
+    field.spawnHero()
+
+
+def updateMoving(key):
+    if key == "a":
+        print("kek")
+        # field.moveLeft()
+    if key == "d":
+        field.moveRight()
+
+
+startGame()
+
+
+try:
+    while True:
+        updateMoving(keyboard.read_key())
+
+except KeyboardInterrupt:
+    pass
 
---
+++ objectModule.py
@@ -0,0 +1,3 @@
+import main
+
+field = main.Field(20, 10)
